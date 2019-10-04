0a241aca61e481c3aef97afce96d08bd50a38846
Yuliia HETMAN <yhetman@student.unit.ua>
2019-10-04 22:18:46 +0300

    | buffer modified |

--- asm/sources/go_to_assembler.c
+++ asm/sources/go_to_assembler.c
@@ -6,19 +6,19 @@
 /*   By: yhetman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/04 20:38:28 by yhetman           #+#    #+#             */
-/*   Updated: 2019/10/04 21:15:03 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/10/04 22:14:03 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
 
-static void	count_lines_and_signs(char *content, int signs, int lines)
+static void	count_lines_and_signs(char *content, int *signs, int *lines)
 {
 	while (content++)
 	{
 		if (*content == '\n')
-			lines++;
-		signs++;
+			(*lines)++;
+		(*signs)++;
 	}
 }
 
@@ -28,14 +28,14 @@ static int	reading_process(char *file, t_reader *reader)
 	int		content;
 	int		fd;
 
-	ft_bzero((void*)buffer, sizeof(char[2]));
-	ft_bzero((void*)reader, sizeof(t_reader*));
+	ft_bzero(buffer, BUFF_SIZE + 1);
+	ft_bzero(reader, sizeof(t_reader*));
 	if ((fd = open(file, O_RDONLY)) == -1)
 		return (-1);
 	while ((content = read(fd, buffer, BUFF_SIZE)) > 0)
 	{
 		buffer[content] = '\0';
-		count_lines_and_signs(buffer, reader->sign, reader->line);
+		count_lines_and_signs(buffer, &reader->sign, &reader->line);
 	}
 	if (content  > -1)
 		lseek(fd, 0, SEEK_SET);
@@ -62,11 +62,11 @@ static int	set_reader(char *file, t_assembler *ass, t_reader *reader, char *line
 int					go_to_assembler(char *file)
 {
 	char			*line;
-	t_header		header;
+//	t_header		header;
 	t_assembler		ass;
 	t_reader		reader;
 
-	if (!set_reader(file, &ass, &reader, file))
+	if (!set_reader(file, &ass, &reader, line))
 		return (0);
 	return (0);
 }
