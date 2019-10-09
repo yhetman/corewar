b41ed3c9ece8a9897a1f97d1c590d018061c6cdc
Yuliia HETMAN <yhetman@student.unit.ua>
2019-10-09 20:52:37 +0300

    | made changes according to the Norme in files that already done |

--- asm/sources/great_freeing.c
+++ asm/sources/great_freeing.c
@@ -6,13 +6,13 @@
 /*   By: yhetman <yhetman@student.unit.ua>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/05 20:59:03 by yhetman           #+#    #+#             */
-/*   Updated: 2019/10/09 20:47:35 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/10/09 20:51:26 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
 
-static void free_commands(char **split)
+static void	free_commands(char **split)
 {
 	int		j;
 
@@ -20,11 +20,11 @@ static void free_commands(char **split)
 	if (!split)
 		return ;
 	while (split[++j])
-		free (split[j]);
+		free(split[j]);
 	free(split);
 }
 
-static void free_stored_info(char ***grid)
+static void	free_stored_info(char ***grid)
 {
 	int		i;
 	int		j;
@@ -56,11 +56,11 @@ static void	free_tokens_name(t_token *tokens, int count)
 	free(tokens);
 }
 
-int	great_freeing(t_assembler *ass, char *line)
+int			great_freeing(t_assembler *ass, char *line)
 {
-	//free_commands(ass->command);
-	// free_stored_info(ass->stored);
-	// free_tokens_name(ass->tokens, ass->count);
+	free_commands(ass->command);
+	free_stored_info(ass->stored);
+	free_tokens_name(ass->tokens, ass->count);
 	ft_strdel(&line);
 	return (0);
 }
