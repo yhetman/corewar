222dc81c571a0d0b856471505fc865c9e309c981
Yuliia HETMAN <yhetman@student.unit.ua>
2019-10-18 21:32:36 +0300

    | get_register in get_command_argument.c |

--- asm/sources/get_command_arguments.c
+++ asm/sources/get_command_arguments.c
@@ -6,11 +6,26 @@
 /*   By: yhetman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/18 19:34:05 by yhetman           #+#    #+#             */
-/*   Updated: 2019/10/18 20:16:12 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/10/18 21:31:47 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
+
+static bool get_register(char *args, t_op options, int index)
+{
+	int		i;
+
+	i = 0;
+	if ((T_REG & options.args_type[index]) != T_REG)
+		return (false);
+	while (args[++i] && args[i] != SEPARATOR_CHAR)
+		if (!IS_DIGIT(args[i]) || i > 3)
+			return (false);
+	if (ft_atoi(args + 1) <= 0 || ft_atoi(args + 1) > REG_NUMBER)
+		return (false);
+	return (true);
+}
 
 static bool	check_amount(char *args, t_op options)
 {
@@ -38,7 +53,7 @@ int		get_command_arguments(t_assembler *ass, t_command *command, int index, int 
 		return (0);
 	while (command->args[i] && arg < ass->options[index].count_args)
 	{
-		if (command->args[i] == 'r') //checking registers 
+		if (command->args[i] == 'r' && !(get_register(command->args + i, ass->options[index], arg))
 			return (0);
 		else if (command->args[i] == DIRECT_CHAR) //checking directs
 			return(0);
