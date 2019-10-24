dbb9a7f354f9d129640ea47f1dba36ede4352378
blukasho <bodik1w@gmail.com>
2019-10-24 12:43:43 +0300

    fix warnings

--- vm/includes/corewar.h
+++ vm/includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/25 06:06:51 by user              #+#    #+#             */
-/*   Updated: 2019/09/25 22:02:42 by user             ###   ########.fr       */
+/*   Updated: 2019/10/24 12:23:35 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define COREWAR_H
 
 # include "../libft/ft_printf/libft/libft.h"
+# include "../libft/ft_printf/ft_printf.h"
 # include "op.h"
 # include <stdlib.h>
 # include <string.h>
@@ -24,7 +25,10 @@
 # include <stdio.h>
 # include <stdbool.h>
 
-# define ABS(X) (((X) < 0) ? (-(X)) : (X))
+/*
+** # define ABS(X) (((X) < 0) ? (-(X)) : (X))
+*/
+
 # define ID(X)				((X) - 1)
 # define PL(i)				cw->player[i]
 
@@ -45,7 +49,7 @@ typedef struct						s_player
 	size_t					prev_lives;
 	ssize_t					last_cycle_check;
 
-};
+}									t_player;
 
 typedef struct						s_carriage
 {
@@ -61,7 +65,7 @@ typedef struct						s_carriage
 	t_player				*player;
 	struct s_carriage			*next;
 
-};
+}									t_carriage;
 
 typedef struct						s_cw
 {
@@ -81,7 +85,7 @@ typedef struct						s_cw
 	ssize_t					cycles_to_die;
 	ssize_t					cycles_after_check;
 
-};
+}									t_cw;
 
 
 
@@ -96,7 +100,7 @@ typedef struct						s_op
 	bool					codage;
 	int						label_size;
 	void					(*func)(t_cw *, t_carriage *, t_op *);
-};
+}									t_op;
 
 t_carriage			*initialize_carriage(t_player *player, int pc);
 void				add_carriage(t_carriage **list, t_carriage *new_carriage);
@@ -120,5 +124,9 @@ int					carry_size(char arg_type, t_op *op);
 unsigned int		update_carry(t_carriage *carriage, t_op *op);
 int					check_arg_types(t_carriage *carriage, t_op *op);
 int					check_args(t_carriage *carriage, t_cw *cw, t_op *op);
+int					parse_args(t_cw *cw, t_carriage *carriage, int arg_num, t_op *op);
+void				int_to_bytecode(unsigned char *map, int position, int value, int size);
+int					bytecode_to_int(const unsigned char *map, int size, int position);
+t_carriage			*clone_carriage(t_carriage *crnt_carriage, int carry);
 
 #endif
