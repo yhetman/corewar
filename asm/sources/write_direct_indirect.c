37aaba790fa75a3b8aef0515ad1534768c2f5b56
yhetman <juliagetman5@gmail.com>
2019-10-24 12:32:17 +0300

    | write_direct_indirect.c | count_bytes | DONE |

--- asm/sources/write_direct_indirect.c
+++ asm/sources/write_direct_indirect.c
@@ -6,14 +6,39 @@
 /*   By: yhetman <yhetman@student.unit.ua>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 11:32:38 by yhetman           #+#    #+#             */
-/*   Updated: 2019/10/24 11:59:46 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/10/24 12:31:43 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/asm.h"
 
-static int	count_bytes(t_assembler *ass, t_writer *writer, char *line, int *hex);
+static int	find_correct_token(t_assembler *ass, char *name)
+{
+	int		index;
 
+	index = -1;
+	while (++index < ass->count)
+		if (!ft_strcmp(ass->tokens[index].name, name))
+			return (index);
+	return (index);
+}
+
+static bool	count_bytes(t_assembler *ass, t_writer *writer, char *line, int *hex)
+{
+	int		i;
+	char	*token;
+
+	i = 0;
+	while (line[i] && line[i] != SEPARATOR_CHAR)
+		i++;
+	if (!(token = ft_strsub(line, 1, i - 1)))
+		return (false);
+	((i = find_correct_token(ass, token)) < 0)
+		return (false);
+	*hex = ass->tokens[i].byte.first - writer->curr_command;
+	free(token);
+	return (true);
+}
 static void	write_direct(t_assembler *ass, t_writer *writer,int *cur, char *line);
 
 static void	write_indirect(t_assembler *ass, t_writer *writer, int *cur, char *line)
