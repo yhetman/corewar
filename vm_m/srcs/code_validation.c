2d1633f5241a47cb03017d5c9324d22388233f7e
Yuliia HETMAN <yhetman@student.unit.ua>
2019-10-27 19:23:05 +0200

    | code_validation and validate_params | DONE |

--- vm_m/srcs/code_validation.c
+++ vm_m/srcs/code_validation.c
@@ -6,11 +6,26 @@
 /*   By: yhetman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/27 18:23:09 by yhetman           #+#    #+#             */
-/*   Updated: 2019/10/27 18:25:03 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/10/27 19:22:14 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+static int	validate_params(t_carriage *carr, t_op *option)
+{
+	char	args_type;
+	int		args;
+
+	args = 0;
+	args_type = read_one_byte(vm, carr->next_op, args);
+	if (option->args_type)
+	{
+		while (++args <= 3)
+			if (option->count_args >= args)
+				memorize_arguments(((char)(args_type & (0x300 / (4 * args))) >> 6 / args), args, carr);
+	}
+}
 
 void	code_calidation(t_vm *vm, t_carriage *carr, t_op *option)
 {
