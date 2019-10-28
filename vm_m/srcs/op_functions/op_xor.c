0bce0c03f0de6afd386a10ba69723a62e08eff6b
Yuliia HETMAN <yhetman@student.unit.ua>
2019-10-28 20:16:22 +0200

    | op_xor | ADDED |

--- vm_m/srcs/op_functions/op_xor.c
+++ vm_m/srcs/op_functions/op_xor.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   op_or.c                                            :+:      :+:    :+:   */
+/*   op_xor.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yhetman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 20:06:50 by yhetman           #+#    #+#             */
-/*   Updated: 2019/10/28 20:16:10 by yhetman          ###   ########.fr       */
+/*   Updated: 2019/10/28 20:15:59 by yhetman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "op_function.h"
 
-void	op_or(t_vm *vm, t_carriage *car)
+void	op_xor(t_vm *vm, t_carriage *car)
 {
 	long	value_1;
 	long	value_2;
@@ -22,7 +22,7 @@ void	op_or(t_vm *vm, t_carriage *car)
 	car->step += 2;
 	value_1 = check_option_params(vm, car, true, 1);
 	value_2 = check_option_params(vm, car, true, 2);
-	value_3 = value_1 | value_2;
+	value_3 = value_1 ^ value_2;
 	car->carry = (bool)(!value);
 	reg = [vm->arena[next_op(car->next_op + car->step)]];
 	car->registers[reg - 1] = value_3;
